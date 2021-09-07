6e1c2837b30ff26a6514384917e68644a05be4fe
sam <1563010155@qq.com>
2021-09-07 22:03:57 +0800

    课程同步提交代码动作

--- video/app/dashboard/views/video.py
+++ video/app/dashboard/views/video.py
@@ -5,7 +5,8 @@ from django.views.generic import View
 from app.libs.base_render import render_to_response
 from app.utils.permission import dashboard_auth
 from app.utils.common import check_and_get_video_type
-from app.model.video import VideoType, FromType, NationalityType, Video
+from app.model.video import \
+    (VideoType, FromType, NationalityType, IdentityType, Video, VideoSub, VideoStar)
 
 
 class ExternaVideo(View):
@@ -57,3 +58,76 @@ class ExternaVideo(View):
 
 
         return redirect(reverse('externa_video'))
+
+
+class VideoSubView(View):
+    TEMPLATE = 'dashboard/video/videosub.html'
+
+    @dashboard_auth
+    def get(self, request, video_id):
+        data = {}
+        video = Video.objects.get(pk=video_id)
+        error = request.GET.get('error', '')
+
+        data['video'] = video
+        data['error'] = error
+
+        return render_to_response(request, self.TEMPLATE, data)
+
+    def post(self, request, video_id):
+        url = request.POST.get('url')
+
+        video = Video.objects.get(pk=video_id)
+
+        length = video.video_sub.count()
+
+        path_format = '{}'.format(reverse('video_sub', kwargs={'video_id': video_id}))
+
+        try:
+            VideoSub.objects.create(video=video, url=url, number=length + 1)
+        except:
+            return redirect('{}?error={}'.format(path_format, '创建失败'))
+
+        return redirect(reverse('video_sub', kwargs={'video_id': video_id}))
+
+
+class VideoStarView(View):
+    
+    def post(self, request):
+        name = request.POST.get('name')
+        identity = request.POST.get('identity')
+        video_id = request.POST.get('video_id')
+
+        path_format = '{}'.format(reverse('video_sub', kwargs={'video_id': video_id}))
+
+        if not all([name, identity, video_id]):
+            return redirect('{}?error={}'.format(path_format, '缺少必要字段'))
+
+        result = check_and_get_video_type(
+            IdentityType, identity, '非法的身份'
+        )
+
+        if result.get('code') != 0:
+            return redirect('{}?error={}'.format(path_format, result['msg']))
+
+        video = Video.objects.get(pk=video_id)
+
+        try:
+            VideoStar.objects.create(
+                video=video,
+                name=name,
+                identity=identity
+            )
+        except:
+            return redirect('{}?error={}'.format(path_format, '创建失败'))
+
+        return redirect(reverse('video_sub', kwargs={'video_id': video_id}))
+
+
+class StarDelete(View):
+
+    def get(self, request, star_id, video_id):
+
+        star = VideoStar.objects.filter(id=star_id).delete()
+
+        return redirect(reverse('video_sub', kwargs={'video_id': video_id}))
